df22537dc320b9a8fc3ff464f2070d0060c19a9a
ryanwong-jetco <ryanwong@jetco.com.hk>
2025-03-10 07:53:35 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,4 @@
+# Here is a new update by ryanwong-jetco
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -9,5 +10,3 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-
- # Here is a new update by ryanwong-jetco
